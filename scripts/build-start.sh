bd1d3909d87bc9f317980fb7c0a470455e2ac2c5
Maxim <mad-maxkozlov@mail.ru>
2022-11-15 01:18:13 +0300

    #1 black magic

--- scripts/build-start.sh
+++ scripts/build-start.sh
@@ -3,3 +3,6 @@
 docker compose build 
 
 ./scripts/start.sh
+
+export WAIT_PORTS=8080,8070,8060,8050 
+./scripts/wait-script.sh
--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -5,7 +5,7 @@ path=$(dirname "$0")
 
 PIDs=()
 for port in "${PORTS[@]}"; do
-  "$path"/wait-for.sh -t 180 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
+  "$path"/wait-for.sh -t 1200 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
 
